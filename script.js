f8a5854df2a67aa95a97095c2afde116997e046d
khalidjambi <khalid.jambi18@gmail.com>
2019-05-06 14:54:48 +0700

    sore

---
+++ script.js
@@ -0,0 +1,7 @@
+var img     = document.getElementById('gambar');
+img.width   = 400;
+
+img.setAttribute('class', 'merah');
+
+var input   = document.getElementById('input');
+input.value = "Nama Lengkap"; 
